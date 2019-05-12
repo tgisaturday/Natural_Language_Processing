50733941fca7056633c8fe572cd6f014f6500c1f
Kim, Taehoon <jamesk1228@gmail.com>
2019-05-12 23:47:50 +0900

    Add files via upload

--- [HW4] Word_Embedding/word_analogy_cbow.py
+++ [HW4] Word_Embedding/word_analogy_cbow.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import torch
 
 f_embed = open("embeddings/CBOW/embedding.pkl","rb")
 f_dict = open("embeddings/CBOW/word_to_ix.pkl","rb")
@@ -12,24 +13,26 @@ f_test = open("data/questions_words.txt","r")
 
 total = 0
 count = 0
-
+unk_idx = word_to_ix["<UNK>"]
 for aLine in f_test:
     if aLine.strip()[0]!= ":" :
         raw = aLine.strip().split()
-        try: 
-            first = embeddings[word_to_ix[raw[0].lower()]]
-        except:
-            first = embeddings[word_to_ix["<UNK>"]]
+        
+        first_idx = word_to_ix.get(raw[0].lower())
+        if first_idx == None:
+            first_idx = unk_idx
+        first = embeddings[first_idx]
+        
+        second_idx = word_to_ix.get(raw[1].lower())
+        if second_idx == None:
+            second_idx = unk_idx
+        second = embeddings[second_idx]
 
-        try: 
-            second = embeddings[word_to_ix[raw[1].lower()]]
-        except:
-            second = embeddings[word_to_ix["<UNK>"]]
-
-        try: 
-            third = embeddings[word_to_ix[raw[2].lower()]]
-        except:
-            third = embeddings[word_to_ix["<UNK>"]]
+        third_idx = word_to_ix.get(raw[2].lower())
+        if third_idx == None:
+            third_idx = unk_idx
+        third = embeddings[third_idx]
+        
         target = first - second + third
 
         
