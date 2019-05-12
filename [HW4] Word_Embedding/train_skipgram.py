d66fb0e82ed9272a16dba4abec78662020501f1f
Kim, Taehoon <jamesk1228@gmail.com>
2019-05-12 22:24:21 +0900

    Add files via upload

---
+++ [HW4] Word_Embedding/train_skipgram.py
@@ -0,0 +1,162 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data_utils
+from torch.utils.data import Dataset, DataLoader
+from collections import Counter
+import time
+import pickle
+import os
+
+torch.manual_seed(1)
+
+EMBEDDING_DIM = 128
+if torch.cuda.is_available():
+  VOCAB_SIZE = 30000
+else:
+  VOCAB_SIZE = 5000
+
+UNK_TOKEN = "<UNK>"
+WINDOW_SIZE = 5
+BATCH_SIZE = 1024
+
+words = []
+with open("data/text8.txt") as f:
+  for line in f.readlines():
+    words += line.strip().split(" ")
+
+print("total words in corpus: %d" % (len(words)))
+
+word_cnt = Counter()
+for w in words:
+  if w not in word_cnt:
+    word_cnt[w] = 0
+  word_cnt[w] += 1
+
+# calculate word coverage of 30k most common words
+total = 0
+for cnt_tup in word_cnt.most_common(VOCAB_SIZE):
+  total += cnt_tup[1]
+print("coverage: %.4f " % (total * 1.0 / len(words)))
+# 95.94%
+
+# make vocabulary with most common words
+word_to_ix = dict()
+for i, cnt_tup in enumerate(word_cnt.most_common(VOCAB_SIZE)):
+  word_to_ix[cnt_tup[0]] = i
+
+# add unk token to vocabulary
+word_to_ix[UNK_TOKEN] = len(word_to_ix)
+
+# replace rare words in train data with UNK_TOKEN
+train_words = []
+for w in words:
+  if w not in word_to_ix:
+    train_words += [UNK_TOKEN]
+  else:
+    train_words += [w]
+
+# make train samples for CBOW
+train_input = []
+train_target = []
+span = (WINDOW_SIZE - 1) // 2
+for i in range(span, len(train_words) - span):
+  context = []
+  for j in range(WINDOW_SIZE):
+    if j != span:
+      context.append(word_to_ix[train_words[i + j - span]])
+  target = word_to_ix[train_words[i]]
+  train_target.append(context)
+  train_input.append(target)
+print("data is generated!")
+
+class SkipGram(nn.Module):
+
+  def __init__(self, vocab_size, embedding_dim, window_size):
+    super(SkipGram, self).__init__()
+    self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+    self.window_size = window_size
+    linears = []
+    for i in range(window_size-1):
+        linears.append(nn.Linear(embedding_dim, vocab_size))
+    self.linears = nn.Sequential(*linears)
+    
+  def forward(self, inputs):
+    embeds = self.embeddings(inputs)
+    log_probs = []
+    for i in range(self.window_size-1):
+        out = self.linears[i](embeds)
+        log_prob =  F.log_softmax(out, dim=1)
+        log_probs.append(log_prob)
+    log_probs = torch.stack(log_probs,dim=2)
+    
+    return log_probs
+
+  def get_word_emdedding(self, word):
+    word = torch.LongTensor([word_to_ix[word]])
+    return self.embeddings(word).view(1, -1)
+
+model = SkipGram(len(word_to_ix), EMBEDDING_DIM, WINDOW_SIZE)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+loss_function = nn.NLLLoss()
+# if gpu is available, then use it
+if torch.cuda.is_available():
+  model.cuda()
+
+# make data loader for batch training
+train_input = torch.from_numpy(np.asarray(train_input)).long()
+train_target = torch.from_numpy(np.asarray(train_target)).long()
+dataset_train = data_utils.TensorDataset(train_input, train_target)
+train_loader = data_utils.DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True, 
+                                     num_workers=4, pin_memory=False)
+
+if not os.path.exists('models/SkipGram/'):
+    os.makedirs('models/SkipGram/')
+if not os.path.exists('embeddings/SkipGram/'):
+    os.makedirs('embeddings/SkipGram/')
+# training loop
+for epoch in range(10):
+  total_loss = 0
+  start = time.time()
+  for i, (context, target) in enumerate(train_loader):
+
+    if torch.cuda.is_available():
+      context = context.cuda()
+      target = target.cuda()
+
+    model.zero_grad()
+
+    log_probs = model(context)
+
+    loss = loss_function(log_probs, target)
+        
+    loss.backward()
+    optimizer.step()
+
+    total_loss += loss.item()
+
+    if (i+1) % 1000 == 0:
+      print("loss: %.4f, steps: %dk" % (loss.item(), ((i+1)/1000)))
+
+  end = time.time()
+  print("epochs: %d" % (epoch+1))
+  print("time eplased: %d seconds" % (end-start))
+  print("mean loss: %.4f" % (total_loss / (train_input.shape[0] // BATCH_SIZE)))
+  torch.save(model.state_dict(), "models/SkipGram/epoch{}.model".format(epoch))
+
+# Here you need to save the model's hidden layer which is V * D word embedding matrix.
+# Then, use the word embedding matrix to get vectors for word
+import pickle
+embedding_matrix = model.embeddings.weight.data
+word_dict = word_to_ix
+
+f_embed = open("embeddings/SkipGram/embedding.pkl","wb")
+pickle.dump(embedding_matrix,f_embed)
+f_embed.close()
+
+f_dict= open("embeddings/SkipGram/word_to_ix.pkl","wb")
+pickle.dump(word_dict,f_dict)
+f_dict.close()
+
---
+++ [HW4] Word_Embedding/word_analogy_skipgram.py
@@ -0,0 +1,49 @@
+import pickle
+import numpy as np
+
+f_embed = open("embeddings/SkipGram/embedding.pkl","rb")
+f_dict = open("embeddings/SkipGram/word_to_ix.pkl","rb")
+
+embeddings = pickle.load(f_embed)
+word_to_ix = pickle.load(f_dict)
+ix_to_word =  {y:x for x,y in word_to_ix.items()}
+
+f_test = open("data/questions_words.txt","r")
+
+total = 0
+count = 0
+
+for aLine in f_test:
+    if aLine.strip()[0]!= ":" :
+        raw = aLine.strip().split()
+        try: 
+            first = embeddings[word_to_ix[raw[0].lower()]]
+        except:
+            first = embeddings[word_to_ix["<UNK>"]]
+
+        try: 
+            second = embeddings[word_to_ix[raw[1].lower()]]
+        except:
+            second = embeddings[word_to_ix["<UNK>"]]
+
+        try: 
+            third = embeddings[word_to_ix[raw[2].lower()]]
+        except:
+            third = embeddings[word_to_ix["<UNK>"]]
+        target = first - second + third
+
+        
+        dist_min = torch.norm(embeddings[0] - target,2)
+        dist_idx = 0
+
+        for i in range(1,len(embeddings)):
+            dist = torch.norm(embeddings[i] - target,2)
+            if dist < dist_min:
+                dist_min = dist
+                dist_idx = i
+        if ix_to_word[dist_idx] == raw[3].lower():
+            count +=1
+        total += 1
+
+print("Word Analogy Task Accuracy: {0:.3f}".format(count/total))
+        
